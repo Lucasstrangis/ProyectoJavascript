ecd07541cbd54eed487b6df38f6295fb9748a24b
Lucas Strangis <lucasstrangis1@gmail.com>
2023-07-29 00:53:34 -0300

    PreEntrega2-Strangis

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ else{
   alert("Esperemos que te encuentres bien " + ingreseNombre + " " + ingreseApellido + ", comencemos!");
 }
 
+let historialOperaciones = [];
+
 function realizarOperacion(operacion, num1, num2) {
   switch (operacion) {
     case "sumar":
@@ -31,6 +33,19 @@ function realizarOperacion(operacion, num1, num2) {
   }
 }
 
+function mostrarHistorial() {
+  console.log("Historial de operaciones:");
+  for (let i = 0; i < historialOperaciones.length; i++) {
+    const operacion = historialOperaciones[i];
+    console.log(
+      `Operación ${i + 1}: ${operacion.num1} ${operacion.operacion} ${operacion.num2} = ${operacion.resultado}`
+    );
+    alert(
+      `Operación ${i + 1}: ${operacion.num1} ${operacion.operacion} ${operacion.num2} = ${operacion.resultado}`
+    );
+  }
+}
+
 let salir = false;
 
 do {
@@ -40,7 +55,7 @@ do {
   console.log("4. Dividir");
   console.log("5. Salir");
 
-  let opcion = parseInt(prompt("Seleccione una opción: 1. Suma || 2. Resta || 3. Multiplicación || 4. División || 5.Salir"));
+  let opcion = parseInt(prompt("Seleccione una opción: 1. Suma || 2. Resta || 3. Multiplicación || 4. División || 5. Historial de operaciones || 6. Salir"));
 
   switch (opcion) {
     case 1:
@@ -69,9 +84,21 @@ do {
       let resultado = realizarOperacion(operacion, num1, num2);
       console.log("El resultado de la operación es " + resultado)
       alert("El resultado de la operación es: " + resultado);
+
+      historialOperaciones.push({
+        num1: num1,
+        operacion: operacion,
+        num2: num2,
+        resultado: resultado,
+      });
+
       break;
 
-    case 5:
+      case 5:
+      mostrarHistorial();
+      break;
+
+    case 6:
       salir = true;
       console.log("Esperamos que te haya servido nuestra ayuda, que tengas un excelente día")
       alert("Esperamos que te haya servido nuestra ayuda, que tengas un excelente día")
